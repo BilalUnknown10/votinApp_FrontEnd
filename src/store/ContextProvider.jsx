09416d015497d966a55f0d2055bd780ff9a89c8b
BilalUnknown10 <bilal9342610@gmail.com>
2024-08-25 16:15:46 +0500

    added api url

--- src/store/ContextProvider.jsx
+++ src/store/ContextProvider.jsx
@@ -7,7 +7,7 @@ function ContextProvider({children}) {
     const [userLogin, setUserLogin] = useState(false);
     const [userName, setUserName] = useState('');
 
-    const API_URL = "http://localhost:3000"
+    const API_URL = "https://vote-app-chi.vercel.app"
 
     const [jipCount, setJipCount] = useState('');
     const [ptiCount, setPtiCount] = useState('');
