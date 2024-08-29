f6df86296b8de999e837c7bbbac84b661a1cdb4b
BilalUnknown10 <bilal9342610@gmail.com>
2024-08-29 15:34:40 +0500

    added react toastify for response alerts

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,9 +3,10 @@ import Navbar from '../components/Navbar'
 import Votebanner from '../components/Votebanner'
 import UserContext from '../store/UserContext'
 
+
 function Home() {
 
-  
+  const {ptiCount,juiCount,anpCount,jipCount,pppCount,pmlNCount} = useContext(UserContext);
 
    
   
@@ -27,6 +28,7 @@ function Home() {
           partyName={'PML-N'}
           chairman={'Nawaz Sharif'}
           partyFlag={"/noon league flag.jfif"}
+          totalVotes={pmlNCount}
           />
 
         <Votebanner
@@ -34,24 +36,28 @@ function Home() {
           partyName={'JIP'}
           chairman={"Hafiz Naeem-Ur-Rahman"}
           partyFlag={'/jumati islami flag.jfif'}
+          totalVotes={jipCount}
         />
         <Votebanner
           leaderImage={'/bilawal butto.jfif'}
           partyName={'PPP'}
           chairman={'Bilawal Bhutto Zardari'}
           partyFlag={'ppp flag.jfif'}
+          totalVotes={pppCount}
           />
         <Votebanner
           leaderImage={'/imran khan.jfif'}
           partyFlag={'pti flag.jfif'}
           partyName={'PTI'}
-          chairman={'Imran Khan'}        />
+          chairman={'Imran Khan'}
+          totalVotes={ptiCount}        />
 
         <Votebanner
           leaderImage={'/aimal wali.jfif'}
           partyFlag={'anp flag.jfif'}
           partyName={'ANP'}
           chairman={'Aimal Wali Khan'}
+          totalVotes={anpCount}
           />
 
         <Votebanner
@@ -59,6 +65,7 @@ function Home() {
           partyFlag={'jui flag.jfif'}
           partyName={"JUI"}
           chairman={'Maulana Fazl-ur-Rehman'}
+          totalVotes={juiCount}
           />
 
     </div>
